81c932939819f3170569c0208b6de49d66bf0885
thelastronnin <ronninsalonga@gmail.com>
2021-06-14 08:40:52 +0800

    Added Form1 & Form2 Components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Form1 from "./components/Form1";
+import Form2 from "./components/Form2";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         render={() => (
           <div>
             <Form1 />
+            <Form2 />
           </div>
         )}
       />
