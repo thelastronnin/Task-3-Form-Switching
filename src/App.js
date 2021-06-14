8b7651ee9788f824aabc1930258f2407a30c5ff2
thelastronnin <ronninsalonga@gmail.com>
2021-06-14 08:39:42 +0800

    Added Form 2 Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Form1 from "./components/Form1";
 
 function App() {
   return (
-    <div>
-      <h3>Hello React World!</h3>
-    </div>
+    <Router>
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <div>
+            <Form1 />
+          </div>
+        )}
+      />
+    </Router>
   );
 }
 
---
+++ src/components/Form2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form1 = () => {
+  return <div>Form 2</div>;
+};
+
+export default Form1;
