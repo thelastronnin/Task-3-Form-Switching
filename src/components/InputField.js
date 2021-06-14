6262d123c38cb04c37fbb9f4611090dc73baea42
thelastronnin <ronninsalonga@gmail.com>
2021-06-14 13:17:14 +0800

    Changed into Class based InputField Component

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,35 +2,39 @@ import React from "react";
 
 // iF = input Field
 
-const InputField = ({
-  iFLabel,
-  iFType,
-  iFPlaceholder,
-  iFvalue,
-  onChange,
-  iFError,
-  iFErrorMessage,
-  maxlength,
-}) => {
-  return (
-    <div>
-      <div className="form-control">
-        <label>{iFLabel}</label>
-        {iFError === true && (
-          <label className="error-message">{iFErrorMessage}</label>
-        )}
+class InputField extends React.Component {
+  render() {
+    const {
+      iFLabel,
+      iFType,
+      iFPlaceholder,
+      iFvalue,
+      onChange,
+      iFError,
+      iFErrorMessage,
+      maxlength,
+    } = this.props;
 
-        <input
-          type={iFType}
-          placeholder={iFPlaceholder}
-          value={iFvalue}
-          onChange={onChange}
-          style={iFError ? { borderColor: "red", borderWidth: "3px" } : {}}
-          maxlength={maxlength}
-        />
+    return (
+      <div>
+        <div className="form-control">
+          <label>{iFLabel}</label>
+          {iFError === true && (
+            <label className="error-message">{iFErrorMessage}</label>
+          )}
+
+          <input
+            type={iFType}
+            placeholder={iFPlaceholder}
+            value={iFvalue}
+            onChange={onChange}
+            style={iFError ? { borderColor: "red", borderWidth: "3px" } : {}}
+            maxlength={maxlength}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default InputField;
