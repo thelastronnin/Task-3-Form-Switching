93d503612bd779f325cc24c357f0ad3151dd526e
thelastronnin <ronninsalonga@gmail.com>
2021-06-14 13:59:41 +0800

    Changed into Class based Form 2 Component

--- src/components/Form2.js
+++ src/components/Form2.js
@@ -5,33 +5,37 @@ import InputFieldTextArea from "./InputFieldTextArea";
 import Button from "./Button";
 
 // iF = INPUT FIELD
+class Form2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fName: "",
+      mName: "",
+      lName: "",
+      bDate: "",
+      address: "",
+      contact: "",
+      email: "",
 
-const Form2 = ({ title, onAdd }) => {
-  const [fName, setFname] = useState("");
-  const [mName, setMname] = useState("");
-  const [lName, setLname] = useState("");
-  const [bDate, setBdate] = useState(null);
-  const [address, setAddress] = useState("");
-  const [contact, setContact] = useState("");
-  const [email, setEmail] = useState("");
+      errorfName: false,
+      errorMName: false,
+      errorLName: false,
+      errorBdate: false,
+      errorAddress: false,
+      errorContact: false,
+      errorEmail: false,
 
-  const [errorfName, setErrorFname] = useState(false);
-  const [errorMName, setErrorMname] = useState(false);
-  const [errorLName, setErrorLname] = useState(false);
-  const [errorBdate, setErrorBdate] = useState(false);
-  const [errorAddress, setErrorAddress] = useState(false);
-  const [errorContact, setErrorContact] = useState(false);
-  const [errorEmail, setErrorEmail] = useState(false);
+      errorMssgFName: "",
+      errorMssgMName: "",
+      errorMssgLName: "",
+      errorMssgBDate: "",
+      errorMssgAddress: "",
+      errorMssgContact: "",
+      errorMssgEmail: "",
+    };
+  }
 
-  const [errorMssgFName, setErrorMssgFName] = useState("");
-  const [errorMssgMName, setErrorMssgMName] = useState("");
-  const [errorMssgLName, setErrorMssgLName] = useState("");
-  const [errorMssgBDate, setErrorMssgBDate] = useState("");
-  const [errorMssgAddress, setErrorMssgAddress] = useState("");
-  const [errorMssgContact, setErrorMssgContact] = useState("");
-  const [errorMssgEmail, setErrorMssgEmail] = useState("");
-
-  const onSubmit = (e) => {
+  onSubmit = (e) => {
     e.preventDefault();
 
     // DO SOMETHING HERE
@@ -39,180 +43,210 @@ const Form2 = ({ title, onAdd }) => {
   };
 
   // CHECK FIRST NAME
-  const onChangeIfFname = (value) => {
-    !value ? setErrorFname(true) : setErrorFname(false);
-    !value ? setErrorMssgFName("Invalid Input") : setErrorMssgFName("");
+  checkFName = (value) => {
+    !value
+      ? this.setState({ errorfName: true })
+      : this.setState({ errorfName: false });
+    !value
+      ? this.setState({ errorMssgFName: "Invalid Input" })
+      : this.setState({ errorMssgFName: "" });
 
     if (/[^a-zA-Z ]/.test(value)) {
-      setErrorFname(true);
-      setErrorMssgFName("A-Z letters only");
+      this.setState({ errorfName: true });
+      this.setState({ errorMssgFName: "A-Z letters only" });
     }
-    return setFname(value);
+    return this.setState({ fName: value });
   };
 
   // CHECK MIDDLE NAME
-  const onChangeIfMname = (value) => {
-    !value ? setErrorMname(true) : setErrorMname(false);
-    !value ? setErrorMssgMName("Invalid Input") : setErrorMssgMName("");
+  checkMName = (value) => {
+    !value
+      ? this.setState({ errorMName: true })
+      : this.setState({ errorMName: false });
+    !value
+      ? this.setState({ errorMssgMName: "Invalid Input" })
+      : this.setState({ errorMssgMName: "" });
 
     if (/[^a-zA-Z ]/.test(value)) {
-      setErrorMname(true);
-      setErrorMssgMName("A-Z letters only");
+      this.setState({ errorMName: true });
+      this.setState({ errorMssgMName: "A-Z letters only" });
     }
-    return setMname(value);
+    return this.setState({ mName: value });
   };
 
   // CHECK LAST NAME
-  const onChangeIfLname = (value) => {
-    !value ? setErrorLname(true) : setErrorLname(false);
-    !value ? setErrorMssgLName("Invalid Input") : setErrorMssgLName("");
+  checkLName = (value) => {
+    !value
+      ? this.setState({ errorLName: true })
+      : this.setState({ errorLName: false });
+    !value
+      ? this.setState({ errorMssgLName: "Invalid Input" })
+      : this.setState({ errorMssgLName: "" });
 
     if (/[^a-zA-Z ]/.test(value)) {
-      setErrorLname(true);
-      setErrorMssgLName("A-Z letters only");
+      this.setState({ errorLName: true });
+      this.setState({ errorMssgLName: "A-Z letters only" });
     }
-    return setLname(value);
+    return this.setState({ lName: value });
   };
 
   // CHECK BIRTHDATE
-  const onChangeIfBdate = (value) => {
-    !value ? setErrorBdate(true) : setErrorBdate(false);
+  checkBDate = (value) => {
+    !value
+      ? this.setState({ errorBdate: true })
+      : this.setState({ errorBdate: false });
 
     if (!value) {
-      setErrorBdate(true);
-      setErrorMssgBDate("Invalid Input");
+      this.setState({ errorBdate: true });
+      this.setState({ errorMssgBDate: "Invalid Input" });
     }
-    return setBdate(value);
+    return this.setState({ bDate: value });
   };
 
   // CHECK ADDRESS
-  const onChangeIfAddress = (value) => {
-    !value ? setErrorAddress(true) : setErrorAddress(false);
-    !value ? setErrorMssgAddress("Invalid Input") : setErrorMssgAddress("");
+  checkAddress = (value) => {
+    !value
+      ? this.setState({ errorAddress: true })
+      : this.setState({ errorAddress: false });
+    !value
+      ? this.setState({ setErrorMssgAddress: "Invalid Input" })
+      : this.setState({ setErrorMssgAddress: "" });
 
     if (/[^a-zA-Z 0-9 ,.-]/.test(value)) {
-      setErrorAddress(true);
-      setErrorMssgAddress("Alphanumeric and (, - .) only");
+      this.setState({ errorAddress: true });
+      this.setState({ setErrorMssgAddress: "Alphanumeric and (, - .) only" });
     }
-    return setAddress(value);
+    return this.setState({ address: value });
   };
 
   // CHECK CONTACT NUMBER
-  const onChangeIfContact = (value) => {
-    !value ? setErrorContact(true) : setErrorContact(false);
-    !value ? setErrorMssgContact("Invalid Input") : setErrorMssgContact("");
+  checkContactNumber = (value) => {
+    !value
+      ? this.setState({ errorContact: true })
+      : this.setState({ errorContact: false });
+    !value
+      ? this.setState({ setErrorMssgContact: "Invalid Input" })
+      : this.setState({ setErrorMssgContact: "" });
+
     if (value.length < 11) {
-      setErrorContact(true);
-      setErrorMssgContact("Invalid Input");
+      this.setState({ errorContact: true });
+      this.setState({ setErrorMssgContact: "Invalid Input" });
     }
 
     if (/[^0-9]/.test(value)) {
-      setErrorContact(true);
-      setErrorMssgContact("Numeric Characters Only");
+      this.setState({ errorContact: true });
+      this.setState({ setErrorMssgContact: "Invalid Input" });
     }
-    return setContact(value);
+    return this.setState({ contact: value });
   };
 
   // CHECK ADDRESS
-  const onChangeIfEmailAddress = (value) => {
-    !value ? setErrorEmail(true) : setErrorEmail(false);
-    !value ? setErrorMssgEmail("Invalid Input") : setErrorMssgEmail("");
+  checkEmailAddress = (value) => {
+    !value
+      ? this.setState({ errorEmail: true })
+      : this.setState({ errorEmail: false });
+    !value
+      ? this.setState({ setErrorMssgEmail: "Invalid Input" })
+      : this.setState({ setErrorMssgEmail: "" });
 
     if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(value)) {
-      setErrorEmail(true);
-      setErrorMssgEmail("Email Address only");
+      this.setState({ errorEmail: true });
+      this.setState({ setErrorMssgEmail: "Email Address only" });
     }
-    return setEmail(value);
+    return this.setState({ email: value });
   };
 
-  return (
-    <form className="add-form">
-      <h3>{title}</h3>
-      {/* FIRST NAME INPUT FIELD */}
-      <InputField
-        iFLabel="First name"
-        iFType="text"
-        iFPlaceholder="First name"
-        iFvalue={fName}
-        onChange={(e) => onChangeIfFname(e.target.value)}
-        iFError={errorfName}
-        iFErrorMessage={errorMssgFName}
-        maxlength="255"
-      />
+  render() {
+    const { title, onAdd } = this.props;
+    return (
+      <form className="add-form">
+        <h3>{title}</h3>
+        {/* FIRST NAME INPUT FIELD */}
+        <InputField
+          iFLabel="First name"
+          iFType="text"
+          iFPlaceholder="First name"
+          iFvalue={this.state.fName}
+          onChange={(e) => this.checkFName(e.target.value)}
+          iFError={this.state.errorfName}
+          iFErrorMessage={this.state.errorMssgFName}
+          maxlength="255"
+        />
 
-      {/* MIDDLE NAME INPUT FIELD */}
-      <InputField
-        iFLabel="Middle name"
-        iFType="text"
-        iFPlaceholder="Middle name"
-        iFvalue={mName}
-        onChange={(e) => onChangeIfMname(e.target.value)}
-        iFError={errorMName}
-        iFErrorMessage={errorMssgMName}
-        maxlength="255"
-      />
+        {/* MIDDLE NAME INPUT FIELD */}
+        <InputField
+          iFLabel="Middle name"
+          iFType="text"
+          iFPlaceholder="Middle name"
+          iFvalue={this.state.mName}
+          onChange={(e) => this.checkMName(e.target.value)}
+          iFError={this.state.errorMName}
+          iFErrorMessage={this.state.errorMssgMName}
+          maxlength="255"
+        />
 
-      {/* LAST NAME INPUT FIELD */}
-      <InputField
-        iFLabel="Last name"
-        iFType="text"
-        iFPlaceholder="Last name"
-        iFvalue={lName}
-        onChange={(e) => onChangeIfLname(e.target.value)}
-        iFError={errorLName}
-        iFErrorMessage={errorMssgLName}
-        maxlength="255"
-      />
+        {/* LAST NAME INPUT FIELD */}
+        <InputField
+          iFLabel="Last name"
+          iFType="text"
+          iFPlaceholder="Last name"
+          iFvalue={this.state.lName}
+          onChange={(e) => this.checkLName(e.target.value)}
+          iFError={this.state.errorLName}
+          iFErrorMessage={this.state.errorMssgLName}
+          maxlength="255"
+        />
 
-      {/* BIRTH DATE INPUT FIELD */}
-      <InputField
-        iFLabel="Birth Date"
-        iFType="date"
-        iFPlaceholder=""
-        iFvalue={bDate}
-        onChange={(e) => onChangeIfBdate(e.target.value)}
-        iFError={errorBdate}
-        iFErrorMessage={errorMssgBDate}
-        maxlength="255"
-      />
+        {/* BIRTH DATE INPUT FIELD */}
+        <InputField
+          iFLabel="Birth Date"
+          iFType="date"
+          iFPlaceholder=""
+          iFvalue={this.state.bDate}
+          onChange={(e) => this.checkBDate(e.target.value)}
+          iFError={this.state.errorBdate}
+          iFErrorMessage={this.state.errorMssgBDate}
+          maxlength="255"
+        />
 
-      {/* ADDRESS INPUT FIELD */}
-      <InputFieldTextArea
-        iFLabel="Address"
-        iFPlaceholder="Address"
-        iFvalue={address}
-        onChange={(e) => onChangeIfAddress(e.target.value)}
-        iFError={errorAddress}
-        iFErrorMessage={errorMssgAddress}
-      />
+        {/* ADDRESS INPUT FIELD */}
+        <InputFieldTextArea
+          iFLabel="Address"
+          iFPlaceholder="Address"
+          iFvalue={this.state.address}
+          onChange={(e) => this.checkAddress(e.target.value)}
+          iFError={this.state.errorAddress}
+          iFErrorMessage={this.state.errorMssgAddress}
+        />
 
-      {/* CONTACT INPUT FIELD */}
-      <InputField
-        iFLabel="Contact Number"
-        iFType="text"
-        iFPlaceholder="09#########"
-        iFvalue={contact}
-        onChange={(e) => onChangeIfContact(e.target.value)}
-        iFError={errorContact}
-        iFErrorMessage={errorMssgContact}
-        maxlength="11"
-      />
+        {/* CONTACT INPUT FIELD */}
+        <InputField
+          iFLabel="Contact Number"
+          iFType="text"
+          iFPlaceholder="09#########"
+          iFvalue={this.state.contact}
+          onChange={(e) => this.checkContactNumber(e.target.value)}
+          iFError={this.state.errorContact}
+          iFErrorMessage={this.state.errorMssgContact}
+          maxlength="11"
+        />
 
-      {/* EMAIL INPUT FIELD */}
-      <InputField
-        iFLabel="Email"
-        iFType="text"
-        iFPlaceholder="email@gmail.com"
-        iFvalue={email}
-        onChange={(e) => onChangeIfEmailAddress(e.target.value)}
-        iFError={errorEmail}
-        iFErrorMessage={errorMssgEmail}
-        maxlength="255"
-      />
+        {/* EMAIL INPUT FIELD */}
+        <InputField
+          iFLabel="Email"
+          iFType="text"
+          iFPlaceholder="email@gmail.com"
+          iFvalue={this.state.email}
+          onChange={(e) => this.checkEmailAddress(e.target.value)}
+          iFError={this.state.errorEmail}
+          iFErrorMessage={this.state.errorMssgEmail}
+          maxlength="255"
+        />
 
-      <Button text="Save" onClick={onSubmit} />
-    </form>
-  );
-};
+        <Button text="Save" onClick={() => this.onSubmit()} />
+      </form>
+    );
+  }
+}
 
 export default Form2;
