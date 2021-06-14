892b0a11d3b0fcacaf978b870b7dbe784bc67ff1
thelastronnin <ronninsalonga@gmail.com>
2021-06-14 13:12:20 +0800

    Changed into Class based Button Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Button = ({ color, text, onClick }) => {
-  return (
-    <button
-      className="btn"
-      style={{ backgroundColor: color }}
-      onClick={onClick}
-    >
-      {text}
-    </button>
-  );
-};
+class Button extends React.Component {
+  render() {
+    const { color, text, onClick } = this.props;
+
+    return (
+      <button
+        className="btn"
+        style={{ backgroundColor: color }}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    );
+  }
+}
 
 Button.defaultProps = {
   color: "green",
