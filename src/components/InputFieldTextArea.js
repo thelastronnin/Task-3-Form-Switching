7847e628d6288ea5f9170a97622d6ace8003b31e
thelastronnin <ronninsalonga@gmail.com>
2021-06-14 13:18:55 +0800

    Changed into Class based InputFieldTextArea Component

--- src/components/InputFieldTextArea.js
+++ src/components/InputFieldTextArea.js
@@ -1,28 +1,32 @@
 import React from "react";
 
-const InputFieldTextArea = ({
-  iFLabel,
-  iFPlaceholder,
-  iFvalue,
-  onChange,
-  iFError,
-  iFErrorMessage,
-}) => {
-  return (
-    <div className="form-control">
-      <label>{iFLabel}</label>
-      {iFError === true && (
-        <label className="error-message">{iFErrorMessage}</label>
-      )}
-      <textarea
-        name="textarea"
-        onChange={onChange}
-        iFvalue={iFvalue}
-        placeholder={iFPlaceholder}
-        style={iFError ? { borderColor: "red", borderWidth: "3px" } : {}}
-      />
-    </div>
-  );
-};
+class InputFieldTextArea extends React.Component {
+  render() {
+    const {
+      iFLabel,
+      iFPlaceholder,
+      iFvalue,
+      onChange,
+      iFError,
+      iFErrorMessage,
+    } = this.props;
+
+    return (
+      <div className="form-control">
+        <label>{iFLabel}</label>
+        {iFError === true && (
+          <label className="error-message">{iFErrorMessage}</label>
+        )}
+        <textarea
+          name="textarea"
+          onChange={onChange}
+          iFvalue={iFvalue}
+          placeholder={iFPlaceholder}
+          style={iFError ? { borderColor: "red", borderWidth: "3px" } : {}}
+        />
+      </div>
+    );
+  }
+}
 
 export default InputFieldTextArea;
