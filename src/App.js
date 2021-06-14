28a7bebbf659979d02921e738beaa59f63a59e69
thelastronnin <ronninsalonga@gmail.com>
2021-06-14 09:01:42 +0800

    Added switch functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Form1 from "./components/Form1";
 import Form2 from "./components/Form2";
 
 function App() {
+  const [switched, setSwitched] = useState(false);
+
+  const toggleSwitch = () => {
+    setSwitched(!switched);
+    alert("This works!" + " " + switched);
+  };
+
   return (
     <Router>
       <Route
         path="/"
         exact
         render={() => (
-          <div>
-            <Form1 />
-            <Form2 />
-          </div>
+          <>
+            <button onClick={toggleSwitch}>Switch</button>
+            {switched ? <Form2 /> : <Form1 />}
+          </>
         )}
       />
     </Router>
